04eadf5f3b6dbd65b6d35b2299bfda884baa77c3
Sumaya Hassan <sumaya_198@hotmail.co.uk>
2021-03-02 11:06:08 +0000

    final changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(logger("dev"));
-//middleware
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Fitness-Tracker',
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
   {
   useNewUrlParser: true,
   useUnifiedTopology: true,
